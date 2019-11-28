ac976b0048e2759b11ede348596d2420d109be60
Dubinin A <keccgroup@gmail.com>
2019-11-28 19:38:40 +0200

    add multi page

--- parser.go
+++ parser.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"github.com/go-bongo/bongo/makehttp"
+	"strconv"
 )
 
 func main()  {
-	makehttp.Scraper("https://www.bjp-online.com", "aside article h3.entry-title")
+	for i := 1; i < 40; i++ {
+		makehttp.Scraper("https://www.bjp-online.com/archive/page/" + strconv.Itoa(i), "aside article h3.entry-title")
+	}
+	//makehttp.Scraper("https://www.bjp-online.com", "aside article h3.entry-title")
 }
